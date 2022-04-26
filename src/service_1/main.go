c7b20367e1cc43119121ed2ea5c442ca9a96a9e5
saghoshprogress <saghosh@progress.com>
2022-04-26 16:34:24 +0530

    Added service 1 for the kube deployment

---
+++ src/service_1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ResponseFromSystem2 struct {
+	message string `json:"message"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to service_1 app",
+		})
+	})
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, this is service_1 app",
+		})
+	})
+	router.GET("/ping-system-2", func(c *gin.Context) {
+		response, err := http.Get("http://service_2:8080/ping")
+
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(responseData))
+		c.JSON(200, string(responseData))
+	})
+	router.Run()
+}
